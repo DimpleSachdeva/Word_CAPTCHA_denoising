994120debfa44368598ba2017f24f97b2c06c42c
DimpleSachdeva <dimple.0726@gmail.com>
2025-08-14 20:02:21 +0530

    scheme_05

---
+++ HWC_5_denoise.py
@@ -0,0 +1,8 @@
+import glob
+import cv2
+for im in glob.glob("D:/16/HW_WCS_5/*"):
+     img = cv2.imread(im.replace('/', '\\'))
+     img = cv2.flip(img, 1)
+     img = cv2.blur(img, (3, 3), 1)
+     _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+     cv2.imwrite("D:\\16\\5_denoised\\" + im[14:], img)
